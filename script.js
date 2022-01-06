3471d054640728d706f965640b49101fb9698e5e
Mikhail Adlin <mikaadlin@hazyBook.local>
2022-01-06 12:19:15 -0500

    added display updating functions

--- script.js
+++ script.js
@@ -11,7 +11,11 @@ function multiply(a,b) {
 };
 
 function divide(a,b) {
-  console.log(a / b);
+  if (b === 0) {
+    console.log("That's not allowed!");
+  } else {
+    console.log(a / b);
+  }
 };
 
 function operate(operator, firstNumber, secondNumber) {
@@ -27,3 +31,47 @@ function operate(operator, firstNumber, secondNumber) {
     console.log('ERROR');
   }
 };
+
+const display = document.querySelector('.display');
+const inputs = document.querySelector('.inputs');
+const results = document.querySelector('.results');
+
+const firstOperand = document.querySelector('.firstOperand');
+const operator = document.querySelector('.operator');
+const secondOperand = document.querySelector('.secondOperand');
+
+document.querySelectorAll('.numbers').forEach(num => {
+  num.addEventListener('click', () => {
+    updateOperands(num.textContent)
+  });
+});
+
+document.querySelectorAll('.operate').forEach(operator => {
+  operator.addEventListener('click', () => {
+    updateOperator(operator.textContent)
+  });
+});
+
+function updateOperands(digit) {
+  if (operator.textContent == '') {
+    firstOperand.textContent += digit;
+  } else {
+      secondOperand.textContent += digit;
+  }
+};
+
+function updateOperator(value) {
+  if (firstOperand.textContent != '' && secondOperand.textContent == '') {
+    operator.textContent = value;
+  } else if (firstOperand.textContent != '' && secondOperand.textContent != '') {
+      let continueCalculation = operate(operator.textContent, firstOperand.textContent, secondOperand.textContent);
+      firstOperand.textContent = continueCalculation;
+      results.textContent = continueCalculation;
+      operator.textContent = value;
+      secondOperand.textContent = '';
+  } else if (firstOperand.textContent == '' && results.textContent != '') {
+      firstOperand.textContent = results.textContent;
+      operator.textContent = value;
+  }
+};
+
